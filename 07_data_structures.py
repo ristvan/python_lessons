6786d7f7220d99586b0a5db2d7fbcd55a6885603
István Rejtő <reistvan@gmail.com>
2025-02-03 16:58:08 +0100

    Extend existing lessons

--- 07_data_structures.py
+++ 07_data_structures.py
@@ -1,46 +1,57 @@
 def lists():
     first_list = [1, 2, 3, 4, 5]
 
-    print(first_list)
-    print(first_list[2])
-    print(first_list[-1])
-    print(first_list[1:3])
-    print(first_list[2:])
-    print(first_list[:3])
-    print(first_list[::2])
-    print(first_list[::-1])
-    print(first_list[1:4:2])
-    print(first_list + [6, 7, 8])
+    print(first_list)  # [1, 2, 3, 4, 5]
+    print(first_list[2])  # 3
+    print(first_list[-1])  # 5
+    print(first_list[1:3]) # [2, 3]
+    print(first_list[2:])  # [3, 4, 5]
+    print(first_list[:3])  # [1, 2, 3]
+    print(first_list[::2])  # [1, 3, 5]
+    print(first_list[::-1])  # [5, 4, 3, 2, 1]
+    print(first_list[1:4:2])  # [2, 4]
+    print(first_list + [6, 7, 8])  # [1, 2, 3, 4, 5, 6, 7, 8]
 
     second_list = list()
+    print(second_list)  # []
+    second_list.append(10)  # [10]
+    print(second_list)  # [10]
+    second_list.extend([11, 12, 13])  # [10, 11, 12, 13]
+    print(second_list) # [10, 11, 12, 13]
+    second_list.insert(1, 20)  # [10, 20, 11, 12, 13]
     print(second_list)
-    second_list.append(10)
-    print(second_list)
-    second_list.extend([11, 12, 13])
-    print(second_list)
-    second_list.insert(1, 20)
-    print(second_list)
-    second_list.remove(11)
+    second_list.remove(11)  # [10, 20, 12, 13]
     print(second_list)
 
 
 def dictionaries():
-    first_dictionary = {"key1": 1, "key2": 2, "key3": 3}
-    print(first_dictionary)
-    print(first_dictionary["key2"])
-    print(first_dictionary.get("key4"))
-    print(first_dictionary.get("key4", 4))
-    print(first_dictionary.keys())
-    print(first_dictionary.values())
-    print(first_dictionary.items())
+    first_dictionary = {
+        "key1": 1,
+        "key2": 2,
+        "key3": 3
+    }
+    print(first_dictionary)  # {'key1': 1, 'key2': 2, 'key3': 3}
+    print(first_dictionary["key2"]) # 2
+    print(first_dictionary.get("key4"))  # None
+    print(first_dictionary.get("key4", 4))  # 4
+    print(first_dictionary.get("key3", 4))  # 3
+    print(first_dictionary.keys())  # ['key1', 'key2', 'key3']
+    print(first_dictionary.values())  # [1, 2, 3]
+    print(first_dictionary.items())  # [('key1', 1), ('key2', 2), ('key3', 3)]
+
+    for key in first_dictionary.keys():
+        print(key, first_dictionary[key])
+
+    for key, value in first_dictionary.items():
+        print(key, value)
 
     second_dictionary = dict()
+    print(second_dictionary)  # {}
+    second_dictionary["key1"] = 10  # {'key1': 10}
     print(second_dictionary)
-    second_dictionary["key1"] = 10
+    second_dictionary.update({"key2": 20, "key3": 30})  # {'key1': 10, 'key2': 20, 'key3': 30}
     print(second_dictionary)
-    second_dictionary.update({"key2": 20, "key3": 30})
-    print(second_dictionary)
-    value = second_dictionary.pop("key2")
+    value = second_dictionary.pop("key2")  # {'key1': 10, 'key3': 30}
     print(value)
     print(second_dictionary)
 
--- 08_list_comprehensation.py
+++ 08_list_comprehensation.py
@@ -4,6 +4,10 @@ def list_comprehension():
     # The list comprehension is a more readable way to create lists.
     l = [i for i in range(10)]  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     print(l)
+    # The list comprehension is equivalent to the following code:
+    # l = list()
+    # for i in range(10):
+    #     l.append(i)
 
     # The general syntax of the list comprehension is:
     #     [expression for item in iterable if condition]
@@ -13,15 +17,40 @@ def list_comprehension():
     # The condition is the filter that is applied to the item.
     l = [i for i in range(10) if i % 2 == 0]  # [0, 2, 4, 6, 8]
     print(l)
-    l = [i ** 2 for i in range(10) if i % 2 == 0]  # [0, 2, 4, 6, 8]
+    # The list comprehension is equivalent to the following code:
+    # l = list()
+    # for i in range(10):
+    #     if i % 2 == 0:
+    #         l.append(i)
+
+    l = [i ** 2 for i in range(10) if i % 2 == 0]  # [0, 4, 16, 36, 64]
     print(l)
+    # The list comprehension is equivalent to the following code:
+    # l = list()
+    # for i in range(10):
+    #     if i % 2 == 0:
+    #         l.append(i ** 2)
 
-    coordinates = [(x, y) for x in range(3) for y in range(3)]
+    coordinates = [(x, y) for x in range(3) for y in range(3)] # [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
     print(coordinates)
+    # The list comprehension is equivalent to the following code:
+    # coordinates = list()
+    # for x in range(3):  # [0, 1, 2]
+    #     for y in range(3):  # [0, 1, 2]
+    #         coordinates.append((x, y))
 
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
     flat_matrix = [number for row in matrix for number in row]
-    print(flat_matrix)
+    print(flat_matrix) # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # The list comprehension is equivalent to the following code:
+    # flat_matrix = list()
+    # for row in matrix:
+    #     for number in row:
+    #         flat_matrix.append(number)
 
 
 
